e91b70c7570ca5bf5b86969eb34dc4d3775b4a5f
Bernardo <54744729+Spitfire720@users.noreply.github.com>
2026-07-03 15:10:37 +0100

    Add legacy FragmentEngine API compatibility shim

---
+++ FragmentEngine_Build1_Source/src/main/java/com/spitfire/fragmentengine/api/FragmentFamily.java
@@ -0,0 +1,8 @@
+package com.spitfire.fragmentengine.api;
+
+public enum FragmentFamily {
+    FRACTURE,
+    ANCHOR,
+    DISTORTION,
+    MEMORY
+}
---
+++ FragmentEngine_Build1_Source/src/main/java/com/spitfire/fragmentengine/api/FragmentState.java
@@ -0,0 +1,8 @@
+package com.spitfire.fragmentengine.api;
+
+public enum FragmentState {
+    STABLE,
+    VEILED,
+    HOLLOWED,
+    UNBOUND
+}
